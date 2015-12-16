4369f4328f948152f86fb99505f02a6a095337b7
smurfik <smurfik@mlpinit.com>
2015-12-16 15:28:57 -0800

    delete unnessesary div

--- list.js
+++ list.js
@@ -2,16 +2,16 @@ window.onload = function () {
 
   var addBtn = document.getElementById("add"),
       input = document.getElementById("input"),
-      sep = document.getElementById("sep"),
       list = document.createElement('ul'),
       li, content;
+
+  document.body.insertBefore(list, null);
 
   var addElement = function (value) {
     li = document.createElement('li');
     content = document.createTextNode(value);
     li.appendChild(content);
     list.appendChild(li);
-    document.body.insertBefore(list, sep);
   };
 
   addBtn.addEventListener("click", function(event) {
